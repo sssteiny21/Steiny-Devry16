ce0020e2b3367d9b8bc1a124e451b63cdfb2b3ae
sssteiny21 <sssteiny21@gmail.com>
2016-11-29 17:19:14 -0500

    SteinyDevrypush

---
+++ jsvictims/victims.js
@@ -0,0 +1,13 @@
+/*var disastervictim = prompt('There has been a disaster. How many victims would you like to enter?')
+if (disastervictim >= 1){
+  prompt('Please enter name');
+  prompt('Please enter phone');
+  propmt('Please enter street address');
+}
+else
+  prompt('Please enter another victim');
+*/
+
+for ( var i=0 ; i <= 500 ; i += 100);
+  console.log(i);
+  
